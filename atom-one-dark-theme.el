4e2f3c6a04eb5199640602b08d6122c053f0cab7
Joel Holdbrooks <cjholdbrooks@gmail.com>
2016-05-13 23:01:45 -0700

    Add support for term

--- atom-one-dark-theme.el
+++ atom-one-dark-theme.el
@@ -260,6 +260,16 @@
 
    ;; flx-ido
    '(flx-highlight-face ((t (:inherit (link) :weight bold))))
+
+   ;; term
+   `(term-color-black ((t :foreground ,atom-one-dark-mono-1)))
+   `(term-color-white ((t :foreground ,atom-one-dark-fg)))
+   `(term-color-blue ((t (:foreground ,atom-one-dark-blue))))
+   `(term-color-green ((t (:foreground ,atom-one-dark-green))))
+   `(term-color-cyan ((t :foreground ,atom-one-dark-cyan)))
+   `(term-color-yellow ((t (:foreground ,atom-one-dark-orange-1))))
+   `(term-color-magenta ((t :foreground ,atom-one-dark-purple)))
+   `(term-color-red ((t :foreground ,atom-one-dark-red-1)))
    ))
 
 ;;;###autoload
